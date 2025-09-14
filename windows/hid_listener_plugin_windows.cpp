8cd1122b694691c268b73ae33cf33906ff5eb44a
dealnotedev <dealnote.dev@gmail.com>
2025-09-14 12:30:49 +0300

    Revert "try fix dart_api_dl conflict"
    This reverts commit 6f88938ed7c859b6b269768b6b844b1e7923830a.

--- windows/hid_listener_plugin_windows.cpp
+++ windows/hid_listener_plugin_windows.cpp
@@ -4,6 +4,7 @@
 #include <flutter/plugin_registrar_windows.h>
 #include <dart-sdk/include/dart_native_api.h>
 #include <dart-sdk/include/dart_api_dl.h>
+#include <dart-sdk/include/dart_api_dl.c>
 
 #include <functional>
 #include "hid_listener_plugin.h"
@@ -12,69 +13,69 @@ static Dart_Port keyboardListenerPort = 0;
 static Dart_Port mouseListenerPort = 0;
 
 void NotifyDart(Dart_Port port, const void* work) {
-    const intptr_t workAddr = reinterpret_cast<intptr_t>(work);
+	const intptr_t workAddr = reinterpret_cast<intptr_t>(work);
 
-    Dart_CObject cObject;
-    cObject.type = Dart_CObject_kInt64;
-    cObject.value.as_int64 = workAddr;
+	Dart_CObject cObject;
+	cObject.type = Dart_CObject_kInt64;
+	cObject.value.as_int64 = workAddr;
 
-    Dart_PostCObject_DL(port, &cObject);
+	Dart_PostCObject_DL(port, &cObject);
 }
 
 static LRESULT KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
-    if (nCode < 0 || keyboardListenerPort == 0 || Dart_PostCObject_DL == nullptr) return CallNextHookEx(NULL, nCode, wParam, lParam);
+	if (nCode < 0 || keyboardListenerPort == 0 || Dart_PostCObject_DL == nullptr) return CallNextHookEx(NULL, nCode, wParam, lParam);
 
-    WindowsKeyboardEventType eventType = WindowsKeyboardEventType::WKE_KeyDown;
+	WindowsKeyboardEventType eventType = WindowsKeyboardEventType::WKE_KeyDown;
 
-    if (wParam == WM_SYSKEYUP || wParam == WM_KEYUP) {
-        eventType = WindowsKeyboardEventType::WKE_KeyUp;
-    }
+	if (wParam == WM_SYSKEYUP || wParam == WM_KEYUP) {
+		eventType = WindowsKeyboardEventType::WKE_KeyUp;
+	}
 
-    KBDLLHOOKSTRUCT* info = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
+	KBDLLHOOKSTRUCT* info = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
 
-    WindowsKeyboardEvent* keyboardEvent = new WindowsKeyboardEvent;
-    keyboardEvent->eventType = eventType;
-    keyboardEvent->vkCode = info->vkCode;
-    keyboardEvent->scanCode = info->scanCode;
+	WindowsKeyboardEvent* keyboardEvent = new WindowsKeyboardEvent;
+	keyboardEvent->eventType = eventType;
+	keyboardEvent->vkCode = info->vkCode; 
+	keyboardEvent->scanCode = info->scanCode;
 
-    NotifyDart(keyboardListenerPort, keyboardEvent);
+	NotifyDart(keyboardListenerPort, keyboardEvent);
 
-    return CallNextHookEx(NULL, nCode, wParam, lParam);
+	return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
 static LRESULT MouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
-    if (nCode < 0 || mouseListenerPort == 0 || Dart_PostCObject_DL == nullptr)
-        return CallNextHookEx(NULL, nCode, wParam, lParam);
+	if (nCode < 0 || mouseListenerPort == 0 || Dart_PostCObject_DL == nullptr)
+		return CallNextHookEx(NULL, nCode, wParam, lParam);
 
-    MouseEventType eventType = MouseEventType::LeftButtonDown;
+	MouseEventType eventType = MouseEventType::LeftButtonDown;
 
-    if (wParam == WM_LBUTTONDOWN) {
-        eventType = MouseEventType::LeftButtonDown;
-    } else if (wParam == WM_LBUTTONUP) {
-        eventType = MouseEventType::LeftButtonUp;
-    } else if (wParam == WM_RBUTTONDOWN) {
-        eventType = MouseEventType::RightButtonDown;
-    } else if (wParam == WM_RBUTTONUP) {
-        eventType = MouseEventType::RightButtonUp;
-    } else if (wParam == WM_MOUSEMOVE) {
-        eventType = MouseEventType::MouseMove;
-    } else if (wParam == WM_MOUSEWHEEL) {
-        eventType = MouseEventType::MouseWheel;
-    } else if (wParam == WM_MOUSEHWHEEL) {
-        eventType = MouseEventType::MouseHorizontalWheel;
-    }
+	if (wParam == WM_LBUTTONDOWN) {
+		eventType = MouseEventType::LeftButtonDown;
+	} else if (wParam == WM_LBUTTONUP) {
+		eventType = MouseEventType::LeftButtonUp;
+	} else if (wParam == WM_RBUTTONDOWN) {
+		eventType = MouseEventType::RightButtonDown;
+	} else if (wParam == WM_RBUTTONUP) {
+		eventType = MouseEventType::RightButtonUp;
+	} else if (wParam == WM_MOUSEMOVE) {
+		eventType = MouseEventType::MouseMove;
+	} else if (wParam == WM_MOUSEWHEEL) {
+		eventType = MouseEventType::MouseWheel;
+	} else if (wParam == WM_MOUSEHWHEEL) {
+		eventType = MouseEventType::MouseHorizontalWheel;
+	}
 
-    MSLLHOOKSTRUCT* info = reinterpret_cast<MSLLHOOKSTRUCT*>(lParam);
+	MSLLHOOKSTRUCT* info = reinterpret_cast<MSLLHOOKSTRUCT*>(lParam);
 
-    MouseEvent *mouseEvent = new MouseEvent;
-    mouseEvent->eventType = eventType;
-    mouseEvent->x = (double)info->pt.x;
-    mouseEvent->y = (double)info->pt.y;
-    mouseEvent->wheelDelta = HIWORD(info->mouseData);
+	MouseEvent *mouseEvent = new MouseEvent;
+	mouseEvent->eventType = eventType;
+	mouseEvent->x = (double)info->pt.x;
+	mouseEvent->y = (double)info->pt.y;
+	mouseEvent->wheelDelta = HIWORD(info->mouseData);
 
-    NotifyDart(mouseListenerPort, mouseEvent);
+	NotifyDart(mouseListenerPort, mouseEvent);
 
-    return CallNextHookEx(NULL, nCode, wParam, lParam);
+	return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
 #if defined(__cplusplus)
@@ -98,28 +99,28 @@ HidListener::~HidListener() {
 #endif
 
 bool SetKeyboardListener(Dart_Port port) {
-    if(HidListener::Get() == nullptr) return false;
-    keyboardListenerPort = port;
-    return true;
+	if(HidListener::Get() == nullptr) return false;
+	keyboardListenerPort = port;
+	return true;
 }
 
 bool SetMouseListener(Dart_Port port) {
-    if(HidListener::Get() == nullptr) return false;
-    mouseListenerPort = port;
-    return true;
+	if(HidListener::Get() == nullptr) return false;
+	mouseListenerPort = port;
+	return true;
 }
 
 void InitializeDartAPI(void* data) {
-    Dart_InitializeApiDL(data);
+	Dart_InitializeApiDL(data);
 }
 
 bool InitializeListeners() {
-    return true;
+	return true;
 }
 
 void HidListenerPluginWindowsRegisterWithRegistrar(
-        FlutterDesktopPluginRegistrarRef registrar) {
-    hid_listener::HidListenerPlugin::RegisterWithRegistrar(
-            flutter::PluginRegistrarManager::GetInstance()
-                    ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
+	FlutterDesktopPluginRegistrarRef registrar) {
+	hid_listener::HidListenerPlugin::RegisterWithRegistrar(
+		flutter::PluginRegistrarManager::GetInstance()
+		->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
 }
